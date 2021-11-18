4020316db32487d2762c6fedacf82cd190cd376f
jhkimnav <jhkimnav@naver.com>
2021-11-18 15:40:17 +0900

    account/signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
 '''
@@ -11,8 +11,3 @@ class CustomUserCreationForm(UserCreationForm):
         model = get_user_model()
         fields = UserCreationForm.Meta.fields + ('profile_img',)
 
-class CustomUserChangeForm(UserChangeForm):
-
-    class Meta:
-        model = get_user_model()
-        fields = ('email', 'first_name', 'last_name',)
