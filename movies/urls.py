b60684d249096ef84bb2b2062c7ac921c5e64461
dongwonKang <dw3624@gmail.com>
2021-11-18 15:42:58 +0900

    movies index, detail

--- movies/urls.py
+++ movies/urls.py
@@ -5,7 +5,9 @@ app_name = 'movies'
 
 urlpatterns = [
     # path('', views.home, name='home'),
-    # path('index', views.index, name='index'),
-    # path('')
-    # path()
+    path('', views.index, name='index'),
+    path('<int:movie_pk>/', views.detail, name='detail'),
+    path('<int:movie_pk>/reviews/create/', views.create_review, name='create_review'),
+    path('<int:movie_pk>/reviews/update/', views.update_review, name='update_review'),
+    path('<int:movie_pk>/reviews/delete/', views.delete_review, name='delete_review'),
 ]
