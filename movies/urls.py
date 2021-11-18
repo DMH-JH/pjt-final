b9ac6cf9a49237b3f51823062d65de10873554fe
dongwonKang <dw3624@gmail.com>
2021-11-18 17:05:15 +0900

    movies err

--- movies/urls.py
+++ movies/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # path('', views.home, name='home'),
     path('', views.index, name='index'),
     path('<int:movie_pk>/', views.detail, name='detail'),
-    path('<int:movie_pk>/reviews/create/', views.create_review, name='create_review'),
-    path('<int:movie_pk>/reviews/update/', views.update_review, name='update_review'),
-    path('<int:movie_pk>/reviews/delete/', views.delete_review, name='delete_review'),
+    path('<int:movie_pk>/review/<int:review_pk>/update/', views.update_review, name='update_review'),
+    path('<int:movie_pk>/review/<int:review_pk>/delete/', views.delete_review, name='delete_review'),
+    path('<int:movie_pk>/review/create/', views.create_review, name='create_review'),
 ]
