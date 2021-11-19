db8dcff5c38c22dc7df65630529edc741282c588
dongwonKang <dw3624@gmail.com>
2021-11-19 13:57:55 +0900

    navbar added

--- movies/forms.py
+++ movies/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Movie_Review
+from .models import Movie_Review, Rank
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Movie_Review
         # fields = '__all__'
+        exclude = ['movie', 'user']
+
+
+class RankForm(forms.ModelForm):
+    class Meta:
+        model = Rank
         exclude = ['movie', 'user']
--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_GET, require_POST, require_http_methods
-from .models import Genre, Movie, Movie_Review
-from .forms import ReviewForm
+from .models import Genre, Movie, Movie_Review, Rank
+from .forms import RankForm, ReviewForm
 
 
 # Create your views here.
@@ -16,7 +16,6 @@ def index(request):
 
 
 def detail(request, movie_pk):
-    movie = get_object_or_404(Movie, pk=movie_pk)
     
     if request.method == 'POST':
         review_form = ReviewForm(request.POST)
@@ -30,10 +29,15 @@ def detail(request, movie_pk):
         review_form = ReviewForm()
         reviews = Movie_Review.objects.filter(movie_id=movie_pk)
     
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    rank = Rank.objects.filter(user=request.user, movie_id=movie_pk)
+    rank_form = RankForm()
     context = {
         'movie': movie,
         'review_form': review_form,
         'reviews': reviews,
+        'rank': rank,
+        'rank_form': rank_form,
     }
     return render(request, 'movies/detail.html', context)
 
@@ -69,3 +73,19 @@ def delete_review(request, movie_pk, review_pk):
     if request.user == review.user and request.method == 'POST':
         review.delete()
     return redirect('movies:detail', movie.pk)
+
+
+def rank_create(request, movie_pk):
+    if request.method == 'POST':
+        rank_form = RankForm(request.POST)
+        if rank_form.is_valid():
+            rank = rank_form.save(commit=False)
+            rank.movie_id = movie_pk
+            rank.user = request.user
+            rank.save()
+            return redirect('movies:detail', movie_pk)
+    return redirect('accounts:login')
+
+
+def rank_update(request, movie_pk, rating_pk):
+    pass
